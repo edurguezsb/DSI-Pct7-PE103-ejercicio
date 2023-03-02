8313e2c7ce0bdb616a7c087c287fcc221f386121
Eduardo Luis Rodriguez San Blas <alu0101128696@ull.edu.es>
2023-03-02 11:06:42 +0000

    ejercicios del PE

--- tests/ejercicio-PE103-2/ejercicio-PE103-2.spec.ts
+++ tests/ejercicio-PE103-2/ejercicio-PE103-2.spec.ts
@@ -15,11 +15,9 @@ describe('Pruebas para la implementación de clases', () => {
 
     it('El método set nombre debe establecer el nombre de la persona', () => {
       const persona = new Persona('Eduardo', 'Rodriguez', new Date(1999, 10, 15), 'M');
-      persona.nombre = 'María';
-      expect(persona.nombre).to.equal('María');
+      persona.nombre = 'Petra';
+      expect(persona.nombre).to.equal('Petra');
     });
-
-    //
   });
 
   describe('Pruebas para la clase Estudiante', () => {
@@ -33,30 +31,26 @@ describe('Pruebas para la implementación de clases', () => {
       estudiante.correoInstitucional = 'Eduardo.rodriguez@ejemplo.com';
       expect(estudiante.correoInstitucional).to.equal('Eduardo.rodriguez@ejemplo.com');
     });
-
-    //
   });
 
   describe('Pruebas para la clase Profesor', () => {
     it('El método get correoInstitucional debe devolver el correo electrónico institucional del profesor', () => {
-      const profesor = new Profesor('María', 'González', new Date(1985, 7, 15), 'F', 'maria.gonzalez@ejemplo.com');
-      expect(profesor.correoInstitucional).to.equal('maria.gonzalez@ejemplo.com');
+      const profesor = new Profesor('Petra', 'Hermosa', new Date(1777, 7, 7), 'F', 'Petra.gonzalez@ejemplo.com');
+      expect(profesor.correoInstitucional).to.equal('Petra.gonzalez@ejemplo.com');
     });
 
     it('El método set correoInstitucional debe establecer el correo electrónico institucional del profesor', () => {
-      const profesor = new Profesor('María', 'González', new Date(1985, 7, 15), 'F', 'maria.gonzalez@ejemplo.com');
-      profesor.correoInstitucional = 'maria.gonzalez2@ejemplo.com';
-      expect(profesor.correoInstitucional).to.equal('maria.gonzalez2@ejemplo.com');
+      const profesor = new Profesor('Petra', 'Hermosa', new Date(1777, 7, 7), 'F', 'Petra.gonzalez@ejemplo.com');
+      profesor.correoInstitucional = 'Petra.gonzalez2@ejemplo.com';
+      expect(profesor.correoInstitucional).to.equal('Petra.gonzalez2@ejemplo.com');
     });
-
-    // 
   });
 
   describe('Pruebas para la clase Asignatura', () => {
     const estudiante1 = new Estudiante('Eduardo', 'Rodriguez', new Date(1999, 10, 15), 'M', 'Eduardo.rodriguez@ejemplo.com');
-    const estudiante2 = new Estudiante('Pepe', 'Benavente', new Date(1992, 9, 25), 'M', 'Pepe.benavente@ejemplo.com');
-    const profesor1 = new Profesor('María', 'González', new Date(1985, 7, 15), 'F', 'maria.gonzalez@ejemplo.com');
-    const profesor2 = new Profesor('Javier', 'Rodríguez', new Date(1978, 2, 10), 'M', 'javier.rodriguez@ejemplo.com');
+    const estudiante2 = new Estudiante('Pepe', 'Benavente', new Date(1500, 1, 1), 'M', 'Pepe.benavente@ejemplo.com');
+    const profesor1 = new Profesor('Petra', 'Hermosa', new Date(1777, 7, 7), 'F', 'Petra.gonzalez@ejemplo.com');
+    const profesor2 = new Profesor('Machu', 'Pichu', new Date(1000, 10, 10), 'M', 'Machu.pichu@ejemplo.com');
     const asignatura = new Asignatura([profesor1, profesor2], [estudiante1, estudiante2]);
 
 
